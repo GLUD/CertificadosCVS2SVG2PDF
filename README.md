cef4438260b6a53710fa30ec13ca728d86937744
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-05-13 20:05:45 -0500

    Se genera un script recursivo

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 DEPENDENCIAS
 
 python 2.7
+
 inkscape 0.91
 
 USO
@@ -12,12 +13,15 @@ Para usarlo bastaría con escribir el comando:
 
 $./SVGtoPDFfromText.py -f Certificado\ Slud\ 2012.svg -t "Nombre del conferencista"
 
-Para usar una lista se puede modificar el archivo SVGtoPDFfromListValues.sh y cambia los nombres de la lista 
-"names" y ejecuta en terminal
+Para usar una lista se puede modificar el archivo SVGtoPDFfromListValues.sh y cambia los nombres de la lista "names" y ejecuta en terminal:
 
 $./SVGtoPDFfromListValues.sh
 
-También se puede utilizar un archivo de valores separados por coma CVS y con sus valores generar los 
-certificados
+También se puede utilizar un archivo de valores separados por coma CSV y con sus valores generar los certificados:
 
-$./SVGtoPDFfromCommaListValuesFile.py -t Certificado\ Slud\ 2012.svg -l lista.cvs 
+$./SVGtoPDFfromCommaListValuesFile.py -t Certificado\ Slud\ 2012.svg -l lista.csv
+
+Si se tiene un CSV con muchos parámetros, se puede usar:
+
+./SVGtoPDFfromCSVWithParams.py -t Certificado\ Cursos.svg -l lista_cursos.csv -n 3
+
---
+++ SVGtoPDFfromCSVWithParams.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import getopt
+
+# Función que lee los parámetros de entrada
+def main(argv):
+    templatefile = ''
+    listfile = ''
+    numparams = 0
+    try:
+        opts, args = getopt.getopt(argv, "h:l:t:n:", ["--template", "--list", "--numparams","template=", "list=", "numparams="])
+    except getopt.GetoptError:
+        print 'Error: SVGtoPDFfromCommaListValuesFile.py -t <template file> -l <list file> -n <num params>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print 'Ayuda: SVGtoPDFfromCommaListValuesFile.py -t <template file> -l <list file> -n <num params>'
+            sys.exit()
+        elif opt in ("-t", "--template"):
+            templatefile = arg
+        elif opt in ("-l", "--list"):
+            listfile = arg
+        elif opt in ("-n", "--numparams"):
+            numparams = int(arg)
+    return [templatefile, listfile, numparams]
+
+templatefile = ''
+listfile = ''
+numparams = 0
+# Lee los parámetros de entrada
+if __name__ == "__main__":
+    [templatefile, listfile, numparams] = main(sys.argv[1:])
+
+# Se abre el archivo separado por comas
+file_handle = open(listfile, "r")
+lista = file_handle.read()
+lista = lista.splitlines()
+file_handle.close()
+
+# Se lee el archivo SVG como XML
+from xml.dom import minidom
+xml_documento = minidom.parse(templatefile)
+nodos = xml_documento.getElementsByTagName("tspan")
+nodosSeleccionados = range(numparams)
+# Los identificadores deben ser puestos previamente
+# en el archivo SVG que sirve como plantilla
+ids = range(numparams)
+
+for x in range(0, numparams):
+    ids[x] = "parametro_" + str(x+1)
+
+for nodo in nodos:
+    for indice in range(0, len(ids)):
+        if nodo.getAttribute("id") == ids[indice]:
+            nodosSeleccionados[indice] = nodo
+
+# Lee las lineas del archivo especificado como lista CVS
+for inputtext in lista:
+    try:
+        parametros = inputtext.split(",")
+        outputfile = ""
+        for indice in range(0, len(parametros)):
+            parametros[indice] = parametros[indice].decode('utf8')
+            nodosSeleccionados[indice].firstChild.replaceWholeText(parametros[indice])
+            outputfile += parametros[indice].replace("\"","")+" - "
+        #Elimina el último espacio
+        outputfile = outputfile[:-3]
+        out_file = "/tmp/output_generarpdf.svg"
+        file_handle = open(out_file, "w")
+        texto = xml_documento.toxml()
+        file_handle.write(texto.encode('utf8'))
+        file_handle.close()
+        outputfile = "Certificado - " + outputfile + ".pdf"
+        print outputfile
+        comando = "inkscape -f \"%s\" -A \"%s\"" % (out_file, outputfile)
+        print comando
+        os.system(comando.encode('utf8'))
+        os.remove(out_file)
+    except Exception:
+        None
+print "¡¡¡Se ha Terminado!!!"
